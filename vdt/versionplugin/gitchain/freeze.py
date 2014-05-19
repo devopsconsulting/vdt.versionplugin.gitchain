5d622c4544ddcfc0d40c073682c791dffa1bac63
Lars van de Kerkhof <lars@permanentmarkers.nl>
2014-05-19 14:14:35 +0200

    Push changes.

--- vdt/versionplugin/gitchain/freeze.py
+++ vdt/versionplugin/gitchain/freeze.py
@@ -11,10 +11,17 @@ def build_package(version):
     Build package with debianize.
     """
     args = parse_version_extra_args(version.extra_args)
+    log.debug("Arguments are {0}".format(args))
 
     log.debug("Building version {0} with gitchain.".format(version))
     with version.checkout_tag:
-        cmd = ['git', 'status'] 
+        if args.force:
+            cmd = ['git', 'push', '--force']
+        else:
+            cmd = ['git', 'push']
+
+        cmd += [args.target_repo, "HEAD:{0}".format(args.target_ref)]
+
         log.debug("Running command {0}".format(" ".join(cmd)))
         log.debug(subprocess.check_output(cmd))
 
--- vdt/versionplugin/gitchain/shared.py
+++ vdt/versionplugin/gitchain/shared.py
@@ -2,8 +2,8 @@ import argparse
 
 def parse_version_extra_args(version_args):
     p = argparse.ArgumentParser(description="Instead of building packages as a freezed state, propagate tags between git repositories.")
-    p.add_argument('--target-repo', help="The full url to the target repository, the tag will be pushed there", nargs=1)
-    p.add_argument('--target-branch', help="The name of the branch to push to.", nargs=1)
-    p.add_argument('--force', '-f', help="Push with --force.")
+    p.add_argument('--target-repo', help="The full url to the target repository, the tag will be pushed there", required=True)
+    p.add_argument('--target-ref', help="A branch name or refspec on the target repository to receive the changes.", required=True)
+    p.add_argument('--force', '-f', help="Push with --force.", action="store_true")
 
     return p.parse_args(version_args)
