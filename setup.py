634ef1d223d87f271feed1d12332b2377d93a0da
Lars van de Kerkhof <lars@permanentmarkers.nl>
2014-05-19 14:44:04 +0200

    Show readme in pypi.

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-# coding: utf-8
+import os
 from setuptools import find_packages, setup
 
 pkgname = "vdt.versionplugin.gitchain"
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(name=pkgname,
       version="0.0.4",
       description="Instead of building packages as a freezed state, propagate tags between git repositories.",
+      long_description=read('README.rst'),
       author="Lars van de Kerkhof",
       author_email="lars@permanentmarkers.nl",
       maintainer="Lars van de Kerkhof",
